a1659da718dc54a90aea4e54ffc70fc528205d9d
Dominik Adam Przybysz <alien11689@gmail.com>
2015-10-08 18:12:46 +0200

    Throw exception when service is not found

--- jigsaw/src/dpr.impl/module-info.java
+++ jigsaw/src/dpr.impl/module-info.java
@@ -1,4 +1,4 @@
 module dpr.impl {
 	requires dpr.api;
-  provides com.dpr.api.IHello with com.dpr.impl.Hello;
+	provides com.dpr.api.IHello with com.dpr.impl.Hello;
 }
--- jigsaw/src/dpr.runner/com/dpr/runner/Main.java
+++ jigsaw/src/dpr.runner/com/dpr/runner/Main.java
@@ -7,8 +7,13 @@ public class Main {
 
   public static void main(String [] args) throws Exception {
     ServiceLoader<IHello> helloServices = ServiceLoader.load(IHello.class);
+    boolean found = false;
     for(IHello helloService : helloServices){
       System.out.println(helloService.helloWorld());
+      found = true;
+    }
+    if(!found) {
+      throw new RuntimeException("HelloService not found");
     }
   }
 }
