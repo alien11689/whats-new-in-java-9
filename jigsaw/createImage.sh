6d7c689f357d3f2062c0ba73d16af2cc73d4f381
Dominik Adam Przybysz <alien11689@gmail.com>
2015-11-21 13:51:25 +0100

    Add impl module to image

--- jigsaw/createImage.sh
+++ jigsaw/createImage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 JAVA_HOME=../..
 
-$JAVA_HOME/bin/jlink --modulepath $JAVA_HOME/jmods:mlib --addmods dpr.runner --output dprApp
+$JAVA_HOME/bin/jlink --modulepath $JAVA_HOME/jmods:mlib --addmods dpr.runner --addmods dpr.impl --output dprApp
